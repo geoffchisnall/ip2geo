092d352060e30eef2e9b29ad60c72796dcc988b0
Geoffrey Chisnall <mooncakeza@gmail.com>
2020-08-04 12:48:06 +0200

    initial upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # ip2geo
+
+##### Summary
+
+- lookup to take an IP or Domain Name and see where it's Geo Location is.
+it is using the https://ipwhois.io API. - Limited to 10000 searches per month.
+-usage
+./ip2geo.py ipaddress")
+example - ./ip2geo.py 8.8.8.8")
+example - ./ip2geo.py www.google.com")
+
+--------------------
+
+#### requirements:
+- python3
+- jq
---
+++ ip2geo.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+import os, sys, subprocess, ipaddress, shutil
+
+def ip_details(rawip):
+
+    try:
+        cmd = "jq"
+        cmdpath = shutil.which(cmd)
+        if cmdpath is None:
+            print("Please install",cmd)
+        else:
+            ip = ipaddress.ip_address(rawip)
+            if ip.is_private:
+                print("This is a Private Address")
+            else:
+                ip = ipaddress.ip_address(rawip)
+                if (ip.version == 4) or (ip.version == 6):
+                    command = 'curl http://ipwhois.app/json/{%s} | jq \'"\(.continent) - \(.country) - \(.org)"\'' % (ip)
+                    runcommand = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+                    result = (runcommand.stdout)
+                    print("%s: %s" % (ip,result))
+    
+    except:
+        lookup = ("host %s | grep 'has address' | awk 'NR==1{print $4}'" % (rawip))
+        lookupresult = subprocess.run(lookup, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        ip=(lookupresult.stdout)
+        ip=ip.strip()
+        command = 'curl http://ipwhois.app/json/{%s} | jq \'"\(.continent) - \(.country) - \(.org)"\'' % (ip)
+        runcommand = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        result = (runcommand.stdout)
+        result = result.strip()
+        print("%s: %s" % (rawip,result))
+
+if __name__ == '__main__':
+    if len(sys.argv) != 1:
+        ip_details(sys.argv[1])
+    else:
+        print("===================")
+        print("=IP to Geolocation=")
+        print("===================")
+        print("Usage: ./ip2geo.py ipaddress")
+        print("example: ./ip2geo.py 8.8.8.8")
+        print("example: ./ip2geo.py www.google.com")
+
+
